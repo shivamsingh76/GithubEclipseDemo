e724ece3585d8a3c3f669e8ee68919cfcc621a73
Shivam Singh <shivam.singh0706@gmail.com>
2022-11-14 12:14:22 +0530

    first commit

---
+++ GithubEclipseDemo/src/com/lti/demos/UserApp.java
@@ -0,0 +1,12 @@
+package com.lti.demos;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e = new Employee(101, "Jay", 50000);
+		
+		System.out.println(e.toString());
+	}
+
+}
